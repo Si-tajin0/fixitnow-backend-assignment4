6ad34763f6a1d685dcae9a4a00ccea722f8d7df2
SI TAJIN <86248181+Si-tajin0@users.noreply.github.com>
2026-08-23 13:54:28 +0600

    test vercel config

--- api/index.ts
+++ api/index.ts
@@ -1,3 +1,14 @@
-import app from "../src/app";
+import express from "express";
+import config from "../src/config";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.json({
+    success: true,
+    message: "Config loaded",
+    environment: config.env,
+  });
+});
 
 export default app;
