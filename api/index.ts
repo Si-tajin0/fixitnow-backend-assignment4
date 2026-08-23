d0f7e8290ccadf65e92805359e36985e08f25d80
SI TAJIN <86248181+Si-tajin0@users.noreply.github.com>
2026-08-23 13:47:41 +0600

    test vercel function

--- api/index.ts
+++ api/index.ts
@@ -1,3 +1,9 @@
-import app from "../src/app";
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Vercel is working!");
+});
 
 export default app;
